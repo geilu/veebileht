d2ffb8f1633c71839120a77a629d4012a9efd300
Kriset Lisette Margareth Laiuste <krisetli@ut.ee>
2024-12-02 03:48:15 +0200

    javascript nuppude efekti jaoks, kommenteeritud

--- javas.js
+++ javas.js
@@ -1,9 +1,15 @@
-// Select all buttons on the page
+// koodi kirjutas Kriset, toetas W3Schools leht süntaksi ja eventidega
+// abi sain ka siit:
+// https://developer.mozilla.org/en-US/docs/Web/API/EventTarget/addEventListener
+
+
+// salvestab kõik nupud lehel
 const buttons = document.querySelectorAll("button");
+// salvestan ka erinupud, millel on erifunktsionaalsus
 const kass = document.querySelector(".kassnupp");
 const koer = document.querySelector(".koernupp");
 
-// Add a "click" event listener to each button
+// lisab klikk sündmuse erinuppudele, mis saadavad alalehtedele
 kass.addEventListener("click", () => {
     window.location.href='kassid.html'
 });
@@ -11,17 +17,29 @@ koer.addEventListener("click", () => {
     window.location.href='koerad.html'
 });
 
-
+// kõikidele nuppudele klikkides suurendab nuppu ning lisab varju
 buttons.forEach((button) => {
     button.addEventListener("click", () => {
-        // Add the "pop-out" effect
+        // efekt ise
         button.style.transform = "scale(1.1)";
-        button.style.boxShadow = "0 4px 8px rgba(0, 0, 0, 0.2)";
+        button.style.boxShadow = "0 4px 8px #FFB300";
 
-        // Remove the effect after a short delay
+        // efekti kadumine
         setTimeout(() => {
             button.style.transform = "scale(1)";
             button.style.boxShadow = "none";
-        }, 200); // Delay in milliseconds
+        }, 200);
     });
+
+    // efekt ka siis, kui hiir nupust üle läheb
+    button.addEventListener("mouseover", () => {
+        button.style.transform = "scale(1.1)";
+        button.style.boxShadow = "0 4px 8px #FFB300";
+    })
+
+    // hiire lahkudes võtab ära
+    button.addEventListener("mouseleave", () => {
+        button.style.transform = "scale(1)";
+        button.style.boxShadow = "none";
+    })
 });
